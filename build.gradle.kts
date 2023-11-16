697e45dd2ef57338a2e24adc8397d87284591aa8
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2023-11-16 14:31:52 +0100

    feat(deps): extract spring version mgt plugin in toml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,40 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	alias(libs.plugins.springBoot)
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+    alias(libs.plugins.springBoot)
+    alias(libs.plugins.springDepMgt)
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
 }
 
 group = "com.bonitasoft.watch.gradle"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
 
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(libs.srpingBootStarter)
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	testImplementation(libs.srpingBootStarterTest)
-	testImplementation(libs.h2)
+    testImplementation(libs.srpingBootStarterTest)
+    testImplementation(libs.h2)
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
